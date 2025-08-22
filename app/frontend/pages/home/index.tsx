27fcdfe725d0a73015f4d40624df9e716b563bd6
Aldrin Dela Cruz <adelacruz0930@gmail.com>
2025-08-23 04:16:01 +0800

    Add logout button

--- app/frontend/pages/home/index.tsx
+++ app/frontend/pages/home/index.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { logoutUser } from '../../state/user/userSlice';
+import { RootState } from '../../state/store';
 
 const HomePage: React.FC = () => {
-  return <h1>This is the home page</h1>
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.user.user);
+
+  const handleLogout = () => {
+    dispatch(logoutUser() as any);
+  };
+
+  return (
+    <div>
+      <h1>This is the home page</h1>
+      {user && user.email && (
+        <p>Welcome, {user.email}!</p>
+      )}
+      <button 
+        onClick={handleLogout}
+        style={{
+          padding: '10px 20px',
+          backgroundColor: '#f44336',
+          color: 'white',
+          border: 'none',
+          borderRadius: '4px',
+          cursor: 'pointer',
+          marginTop: '20px'
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
 }
 
 export default HomePage;
