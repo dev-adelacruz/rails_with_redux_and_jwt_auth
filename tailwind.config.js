649ae0221d87d6c0b5855e523a9a279afbd8649b
Aldrin Dela Cruz <adelacruz0930@gmail.com>
2025-09-28 12:06:00 +0800

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    "./app/frontend/**/*.{html,js,jsx,ts,tsx}", // Update according to your project file structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
